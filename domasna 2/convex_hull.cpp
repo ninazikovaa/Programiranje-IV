e3a020f6ee21d242e2ad31a5249f1381a1ad2723
ninazikovaa <157800625+ninazikovaa@users.noreply.github.com>
2025-03-27 10:07:39 +0100

    convex_hull.cpp

--- domasna 2/convex_hull.cpp
+++ domasna 2/convex_hull.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Point {
+struct Point
+{
     int x, y;
-    Point(int x1, int y1) {
+    Point(int x1, int y1) 
+    {
         x = x1;
         y = y1;
     }
@@ -15,25 +17,30 @@ struct Point {
     }
 };
 
-bool dali_desno(Point p, Point q, Point r) {
+bool dali_desno(Point p, Point q, Point r) 
+{
     int det=(q.x - p.x)*(r.y-p.y)-(q.y-p.y)*(r.x-p.x);
     return det<=0; 
 }
 
-int main() {
+int main()
+{
     int n;
     cin>>n;
     vector<Point> points;
     set<pair<Point, Point>> edges; 
     
-    for (int i=0; i<n;i++) {
+    for (int i=0; i<n;i++) 
+    {
         int x, y;
         cin>>x>>y;
         points.push_back(Point(x, y));
     }
     
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
+    for (int i=0; i<n; i++) 
+    {
+        for (int j=0; j<n; j++) 
+        {
             if (i==j) 
                 continue;
             
@@ -54,13 +61,15 @@ int main() {
                 }
             }
             
-            if (valid) {
+            if (valid) 
+            {
                 edges.insert({p, q}); 
             }
         }
     }
     
-    for (const auto& edge : edges) {
+    for (const auto& edge : edges) 
+    {
         Point a=edge.first;
         Point b=edge.second;
         cout<<"("<<a.x<<", "<<a.y<<") -> ("<<b.x<<", "<<b.y<<")<<endl;
